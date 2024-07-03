fbc8c246db85e9a45374c7b899da6c0d1b99d7fb
arthurW1935 <sourashis.23bcs10038@ms.sst.scaler.com>
2024-07-03 10:09:51 +0530

    added get post put delete requests

--- express/index.js
+++ express/index.js
@@ -1,14 +1,42 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 let courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
-    { id: 3, name: 'course3' }
+    { id: 3, name: 'course3' },
 ];
 
 app.get('/courses', (req, res) => {
     res.json(courses);
+});
+
+app.post('/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.json(course);
+});
+
+app.put('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+    course.name = req.body.name;
+    res.json(course);
+});
+
+app.delete('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.json(course);
 });
 
 
