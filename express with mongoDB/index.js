2b052dfcc6c71b286198dcf7fafabca1797987c2
arthurW1935 <sourashis.23bcs10038@ms.sst.scaler.com>
2024-07-08 12:25:35 +0530

    added controller and routes for user

--- express with mongoDB/index.js
+++ express with mongoDB/index.js
@@ -1,61 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const productRoutes = require('./routes/productRoutes')
+const userRoutes = require('./routes/userRoutes')
+require('dotenv').config()
 
-
-const mongoURI = process.env.MONGO_URI;
-mongoose
-    .connect(mongoURI)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.log("MongoDB connection failed due to an error: ", err);
-    });
-
-
-const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    stock: {
-        type: Number,
-        required: true
-    },
-    category: {
-        type: String,
-        required: true
-    }
-});
-
-const productModel = mongoose.model("Products", productSchema);
 
 const app = express();
 app.use(express.json());
-const PORT = 3000;
 
-app.post("/api/products", async (req, res) => {
-    try {
-        const product = await productModel.create({
-            name: req.body.name,
-            price: req.body.price,
-            stock: req.body.stock,
-            category: req.body.category
-        });
-        res.status(201).json({'message': 'Product created successfully'});
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Db Connected");
+  })
+  .catch((err) => {
+    console.log("Db connection Failed", err);
+  });
 
 
+app.use('/api/products' , productRoutes)
+app.use('/api/users' , userRoutes)
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(8086, () => {
+  console.log("Server sarted at port 8086");
 });
