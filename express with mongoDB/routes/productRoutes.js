d664f98762e4516fa0ec066b5eb3823640d6d947
arthurW1935 <sourashis.23bcs10038@ms.sst.scaler.com>
2024-07-08 11:35:32 +0530

    added product router

---
+++ express with mongoDB/routes/productRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const productControllers = require("../controllers/productControllers");
+
+
+router.post("/", productControllers.createProduct);
+
+router.get("/", productControllers.getallProducts);
+
+router.get("/:id", productControllers.getById);
+
+router.put("/:id", productControllers.updateProduct);
+
+router.delete("/:id", productControllers.deleteProduct);
+
+
+module.exports = router
